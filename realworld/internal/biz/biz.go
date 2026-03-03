84ccce41ec401267bf68f442aa0e2b7d16f90f5f
17331527@qq.com <173315279@qq.com>
2026-03-03 21:11:28 +0800

    git

--- realworld/internal/biz/biz.go
+++ realworld/internal/biz/biz.go
@@ -3,10 +3,4 @@ package biz
 import "github.com/google/wire"
 
 // ProviderSet is biz providers.
-// 虽然当前项目的依赖注入是通过 wire_gen.go 手动生成好的，
-// 但仍然在 ProviderSet 中补充 TodoUsecase，方便后续如果重新生成 wire 代码时自动加入。
-var ProviderSet = wire.NewSet(
-	NewGreeterUsecase,
-	NewArticleUseCase,
-	NewTodoUsecase,
-)
+var ProviderSet = wire.NewSet(NewGreeterUsecase, NewArticleUseCase)
